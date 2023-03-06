93ec43e6adc20db516fc41d733aa217eeb363eb8
Anish <aniish9.18@gmail.com>
2023-03-06 23:06:49 +0530

    roman to integer

---
+++ romantointeger.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        h = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = 0
+        for i in range(len(s)):
+            if i < len(s) - 1 and h[s[i]] < h[s[i + 1]]:
+                res -= h[s[i]]
+            else:
+                res += h[s[i]]
+        return res
