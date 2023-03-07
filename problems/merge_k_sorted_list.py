bdd2393764f5102f02f27fac759637f9d0ec25e0
Anish <aniish9.18@gmail.com>
2023-03-07 14:14:18 +0530

    Folders Created

---
+++ problems/merge_k_sorted_list.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        k = len(lists)
+        if k == 0:
+            return None
+        if k == 1:
+            return lists[0]
+        merge_list = lists[0]
+        for i in range(1, k):
+            merge_list = self.mergeTwoLists(merge_list, lists[i])
+        return merge_list
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
